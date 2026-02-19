1b2cdcf33688ee20571e6029be2a03872b2eb5ec
Gent Veseli <gent.veseli1@gmail.com>
2026-02-19 11:41:59 +0100

    deleted, and starting from scratch

--- src/app/[slug]/page.tsx
+++
@@ -1,5 +0,0 @@
-import LandingClient from "../components/landing/LandingClient";
-
-export default function Page({ params }: { params: { slug: string } }) {
-  return <LandingClient slug={params.slug} />;
-}
